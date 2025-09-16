281f9ca94ba47909a68faf10d3d715c71200707a
Zaki <145068764+onlyzaki@users.noreply.github.com>
2025-09-16 22:28:54 +0100

    Update Main.java

--- simpleCalculator/src/Main.java
+++ simpleCalculator/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.HashSet;
 import java.util.Set;
@@ -7,60 +8,118 @@ public class Main {
         Set<String> set = new HashSet<>();
         set.add("break");
         set.add("stop");
-        double num1 = 0;
-        double num2;
+        int operations;
+        double[] numbers;
+        String afterNum;
         double result = 0;
         char operator;
         String temp;
-        while (true){
-            System.out.print("Enter the first number: ");
-            temp = scanner.next().replace(',','.');
-            if (set.contains(temp)){
-                break;
-            }
-            if (temp.equals("ans")){
-                num1 = result;
-            }else {
-                try {
-                    num1 = Double.parseDouble(temp);
-                } catch (NumberFormatException e) {
-                    System.out.println("Invalid input, accepts only numbers!");
-                    continue;
-                }
-            }
-            System.out.print("Enter the operator + | - | * | / | ^ : ");
-            operator = scanner.next().charAt(0);
-
-            System.out.print("Enter the second number: ");
-            temp = scanner.next().replace(',','.');
-            if (set.contains(temp)){
+        String newCheck;
+        boolean zeroDiv;
+        boolean valid = true;
+        while (valid){
+            zeroDiv = true;
+            System.out.print("how much numbers do you wanna operate: ");
+            newCheck = scanner.next();
+            if (set.contains(newCheck)){
                 break;
             }
             try {
-                num2 = Double.parseDouble(temp);
+                operations = Integer.parseInt(newCheck);
             }catch (NumberFormatException e){
-                System.out.println("Invalid input, accepts only numbers!");
+                System.out.println("Invalid number, try again");
+                scanner.nextLine();
                 continue;
             }
-            switch (operator) {
-                case '+' -> result = num1 + num2;
-                case '-' -> result = num1 - num2;
-                case '*' -> result = num1 * num2;
-                case '/' -> {
-                    if (num2 == 0) {
-                        System.out.println("Cannot divide by zero, try again!");
-                        continue;
-                    } else {
-                        result = num1 / num2;
+            if (operations < 0){
+                System.out.println("doesn't accept negative numbers, try again");
+                continue;
+            } else if (operations > 15) {
+                System.out.println("too big of an operation, try again");
+                continue;
+            }
+            numbers = new double[operations];
+
+            System.out.print("Enter the operator + | - | * | / | ^ : ");
+            operator = scanner.next().charAt(0);
+            boolean condition = operator == '+' || operator == '-' || operator == '*' || operator == '/' || operator == '^';
+            if (!condition){
+                System.out.println("Invalid operator, try again!");
+                continue;
+            }
+
+            for(int i = 1; i < operations + 1; i++){
+                switch (i){
+                    case 1 ,11-> afterNum = "st";
+                    case 2 ,12-> afterNum = "nd";
+                    case 3 ,13-> afterNum = "rd";
+                    default -> afterNum = "th";
+                }
+                System.out.printf("Enter the %d%s number: ", i, afterNum);
+                temp = scanner.next().replace(',','.');
+                if (set.contains(temp)){
+                    valid = false;
+                    break;
+                }
+                if(temp.equals("ans")){
+                    numbers[i -1] = result;
+                }else {
+                    try {
+                        numbers[i -1] = Double.parseDouble(temp);
+                    }catch (NumberFormatException e){
+                        System.out.println("Invalid input, accepts only numbers!");
+                        i--;
                     }
                 }
-                case '^' -> result = Math.pow(num1, num2);
+            }
+            if (!valid){
+                break;
+            }
+            result = numbers[0];
+            switch (operator){
+                case '+' ->{
+                    result = 0;
+                    for (double num : numbers){
+                        result += num;
+                    }
+                }
+                case '-' ->{
+                    for (int i = 1; i < numbers.length; i++){
+                        result -= numbers[i];
+                    }
+                }
+                case '*' ->{
+                    for (int i = 1; i < numbers.length; i++){
+                        result *= numbers[i];
+                    }
+                }
+                case '/' ->{
+                    for (double check : numbers){
+                        if (check == 0){
+                            System.out.println("can't devide by zero");
+                            zeroDiv = false;
+                            break;
+                        }
+                    }
+                    if (zeroDiv){
+                        for (int i = 1; i < numbers.length; i++){
+                            result /= numbers[i];
+                        }
+                    }
+
+                }
+                case '^' ->{
+                    result = numbers[numbers.length - 1];
+                    for (int i = numbers.length - 2; i >= 0; i--){
+                        result = Math.pow(numbers[i], result);
+                    }
+                }
                 default -> {
                     System.out.println("Invalid operator, try again!");
                     continue;
                 }
             }
-            System.out.println(result);
+            System.out.printf("This is the result: %,.3f\n", result);
         }
         System.out.println("Thank you for using my app! Made by onlyzaki.");
         scanner.close();
